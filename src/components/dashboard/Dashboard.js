0bd728fc1df4162b6ae2226e6dce67a57f774d8c
Semilore Idowu <semijacks27@gmail.com>
2020-03-25 23:08:34 +0100

    redux

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -5,11 +5,14 @@ import { connect } from "react-redux";
 
 class Dashboard extends Component {
   render() {
+    // console.log(this.props);
+    const { projects } = this.props;
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -20,4 +23,10 @@ class Dashboard extends Component {
   }
 }
 
-export default connect()(Dashboard);
+const mapStateToProps = state => {
+  return {
+    projects: state.project.projects
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
